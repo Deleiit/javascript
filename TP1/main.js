970d8ce3aff719c76b92674fd6852256c28659fb
Deleiit <pcsbdwmm@duck.com>
2024-11-25 14:31:56 +0100

    V1

---
+++ TP1/main.js
@@ -0,0 +1,26 @@
+// TP - Consommation
+function conso() {
+    const essence =document.getElementById('essence').value;
+    const km =document.getElementById('km').value;
+    const conso =km*essence/100
+    document.getElementById('conso').innerHTML = conso;
+    document.getElementById('alert').classList.remove('alert-success');
+    document.getElementById('alert').classList.remove('alert-warning');
+    document.getElementById('alert').classList.remove('alert-danger');
+    if (conso < 6) {
+        document.getElementById('alert').classList.add('alert-success')
+        document.getElementById('conseil').innerHTML = "Vous respectez l'environnement !"
+    }
+    else if (6 > conso > 10) {
+        document.getElementById('alert').classList.add('alert-warning')
+        document.getElementById('conseil').innerHTML = "Il va faire tout noir !"
+    }
+    else if (conso > 10) {
+        document.getElementById('alert').classList.add('alert-danger')
+        document.getElementById('conseil').innerHTML = "Jeanne au secours !"
+    }
+    document.getElementById('btnEnlever').onclick = (event) =>{
+        const selecteur = event.target;
+        selecteur.remove();
+    }
+}
---
+++ TP2/main.js
@@ -0,0 +1,5 @@
+// TP - Fruits
+function fruit() {
+    const nom =document.getElementById('fruit').value;
+    console.log(nom)
+}
